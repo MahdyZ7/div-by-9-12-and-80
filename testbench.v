ea1f66748803591f4a867c8291626a1d5c597db5
Ahmed Mahdy <amahdy9@gmail.com>
2023-03-26 14:37:25 +0000

    div by 80 ready for synth

--- testbench.v
+++ testbench.v
@@ -2,16 +2,11 @@ module testbench();
 	reg clk;
 	output wire q;
 	output reg rst;
-	output wire q1;
-	output wire q2;
 	output wire [6:0]clk_rx;
 	
 
 	// int i = 0;
-	div_by_2 d2 (.rst_n(rst), .clk_rx(clk), .clk_tx(q));
 	div_by_80_50dut dut (.reset(rst),.clk(clk), .out(q));
-	div_by_9_50dut dut2 (.reset(rst),.clk(clk), .out(q2));
-	div_by_12_50dut dut3 (.reset(rst),.clk(clk), .out(q1));
 	counter counter (.rst(rst), .clk(clk), .counter(count));
 	initial begin
 		rst = 0;
@@ -21,9 +16,9 @@ module testbench();
 	
 	initial begin
 		$dumpvars;
-		$monitor("Time: %0d ns, clk: %b, q: %b q1: %b", $time, clk, q, q1);
+		$monitor("Time: %0d ns, clk: %b, q: %b", $time, clk, q);
 		rst =0;
 		#2 rst =1;
-	  #100 $finish;
+	  #170 $finish;
 	end
 endmodule
